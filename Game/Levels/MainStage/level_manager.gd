122c13e5090044e8b6ffec5c5e9e7b70d26f7245
Richard Miikael Jaks <richardmiikael.jaks@gmail.com>
2025-01-29 19:51:08 +0200

    Refactored level_manager.gd

--- Game/Levels/MainStage/level_manager.gd
+++ Game/Levels/MainStage/level_manager.gd
@@ -16,6 +16,8 @@ var player_2: Character
 
 var volleyball: Volleyball
 
+@onready var tree: SceneTree = get_tree()
+
 @onready var player_1_pos: Marker2D = %Player1Position
 @onready var player_2_pos: Marker2D = %Player2Position
 
@@ -23,13 +25,14 @@ var volleyball: Volleyball
 @onready var vb_2_pos: Marker2D = %VolleyballP2Position
 
 # UI
-@export var transition: Control
+@onready var fader: Control = %Fader
 @export var dialog_box: DialogBox
 #endreigon
 
 # Audio
 @onready var a_hit_land: AudioStreamPlayer = %HitLand
 @onready var a_hit_water: AudioStreamPlayer = %HitWater
+@onready var a_crickets: AudioStreamPlayer = %Crickets
 
 # AI Helpers
 @onready var midpoint: Marker2D = %ArenaMidpointHelper
@@ -44,8 +47,21 @@ var right_health: int = PlayerVars.total_health
 @export var is_pvp: bool = false
 #endregion
 
+#region Visuals
+var fade_time: float = 2
+var post_fade_delay: float = 1
+#endregion
 
 func _ready() -> void:
+	tree.paused = true
+	_initialize_players()
+	_reset_player_positions()
+
+	var fade: Tween = _fade_in(fade_time)
+	fade.tween_callback(_start_level.bind(post_fade_delay))
+
+#region Level initialization
+func _initialize_players() -> void:
 	left_health = PlayerVars.total_health
 	right_health = PlayerVars.total_health
 
@@ -57,99 +73,31 @@ func _ready() -> void:
 	player_2.side = PlayerVars.SIDE.RIGHT	
 	add_child(player_2)
 
-	_reset_player_positions()
-	var t = %Fader.create_tween()
-
-	t.tween_property(%Fader, "color:a", 0, 2).set_ease(Tween.EASE_IN)
+func _start_level(delay: float) -> void:
+	var next_action_timer: SceneTreeTimer = \
+		tree.create_timer(delay, true, false, true)
+	
 	if dialog_box:
-		t.tween_callback(func(): get_tree().create_timer(1).timeout.connect(func(): 
-			dialog_box.visible = true
-			dialog_box.process_mode = Node.PROCESS_MODE_ALWAYS
-			))
-		
-		dialog_box.finished.connect(_initialize_game)
+		next_action_timer.timeout.connect(
+			func():
+				dialog_box.visible = true
+				dialog_box.process_mode = Node.PROCESS_MODE_ALWAYS
+		)
+		dialog_box.finished.connect(_start_game)
 	else:
-		t.tween_callback(func(): get_tree().create_timer(1).timeout.connect(_initialize_game))
+		next_action_timer.timeout.connect(_start_game)
 
-func _check_collision(body: CollisionObject2D) -> void:
-	if not (body is Ground):
-		return
-	
-	a_hit_land.play()
-	_create_stuck_ball(volleyball, ps_stuck_vb)
-		
-	_reduce_health(body.side)
-	SignalBus.landed.emit()
-
-func _create_stuck_ball(vb: Volleyball, ps: PackedScene) -> void:
-	var pos = vb.global_position
-	var stuck_vb = ps.instantiate()
-	add_child(stuck_vb, true)
-
-	vb.queue_free()
-	stuck_vb.global_position = pos	
-
-func _reduce_health(id: PlayerVars.SIDE) -> void:
-	match(id):
-		PlayerVars.SIDE.LEFT:
-			left_health -= 1
-		PlayerVars.SIDE.RIGHT:
-			right_health -= 1
-
-	# Win
-	if right_health <= 0:
-		winner = PlayerVars.SIDE.LEFT
-		if is_pvp:
-			_show_who_won()
-			return
-		get_tree().create_timer(3, true, false, true) \
-			.timeout.connect(do_transition)
-		get_tree().paused = true
-		return
-	# Lose
-	if left_health <= 0:
-		winner = PlayerVars.SIDE.RIGHT
-		get_tree().paused = true
-		if not is_pvp:
-			_show_game_over()
-		else:
-			_show_who_won()
-		return
-	
-	%Crickets.playing = true
-	_initialize_game()	
-
-func _show_game_over() -> void:
-	%GameOver.visible = true
-
-func _show_who_won() -> void:
-	%WhichWon.set_win_text(winner)
-	%WhichWon.visible = true
-
-func do_transition() -> void:
-	var t = %Fader.create_tween()
-	t.tween_property(%Fader, "color:a", 1, 2).set_ease(Tween.EASE_OUT)
-	
-	t.tween_callback(
-		func():
-			get_tree().paused = false
-			get_tree().change_scene_to_file(next_screen)
-	)
-
-func _initialize_game() -> void:
+func _start_game() -> void:
 	if dialog_box:
 		dialog_box.visible = false
 		dialog_box.process_mode = Node.PROCESS_MODE_DISABLED
 
-	get_tree().paused = true
-	get_tree().create_timer(3, true, false, true) \
-		.timeout.connect(
+	tree.create_timer(3, true, false, true).timeout.connect(
 		func(): 
-			get_tree().paused = false
-			%Crickets.playing = false
+			tree.paused = false
 			SignalBus.game_continued.emit()
-
 	)
+
 	_reset_player_positions()
 	volleyball = _spawn_volleyball(ps_volleyball)
 	
@@ -165,6 +113,96 @@ func _reset_player_positions() -> void:
 	player_2.global_position = player_2_pos.global_position
 	player_1.animator.play("idle")
 	player_2.animator.play("idle")
+#endregion
+
+#region Fading tweens
+func _fade_in(tween_time: float) -> Tween:
+	var tween: Tween = fader.create_tween()
+	tween.tween_property(fader, "color:a", 0, tween_time)\
+		.set_ease(Tween.EASE_IN)
+	
+	return tween
+
+func _fade_out(tween_time: float) -> Tween:
+	var tween: Tween = fader.create_tween()
+	tween.tween_property(fader, "color:a", 1, tween_time)\
+		.set_ease(Tween.EASE_OUT)
+	
+	return tween
+#endregion
+
+#region Game runtime handling
+func _reduce_health(id: PlayerVars.SIDE) -> void:
+	tree.paused = true
+	match(id):
+		PlayerVars.SIDE.LEFT:
+			left_health -= 1
+		PlayerVars.SIDE.RIGHT:
+			right_health -= 1
+	
+	_check_health()
+	
+func _check_health() -> void:
+	if left_health <= 0:
+		_handle_win(PlayerVars.SIDE.RIGHT)
+		return
+	if right_health <= 0:
+		_handle_win(PlayerVars.SIDE.LEFT)
+		return
+	
+	a_crickets.play()
+	_start_game()
+	
+func _handle_win(winning_side: PlayerVars.SIDE) -> void:
+	winner = winning_side
+
+	if is_pvp:
+		_show_who_won()
+		return
+	
+	if winner == PlayerVars.SIDE.LEFT:
+		var fade: Tween = _fade_out(fade_time)
+		fade.tween_callback(_switch_to_scene.bind(next_screen))
+	if winner == PlayerVars.SIDE.RIGHT:
+		_show_game_over()
+#endregion
+
+#region UI
+func _show_who_won() -> void:
+	%WhichWon.visible = true
+	%WhichWon.set_win_text(winner)
+	tree.paused = true
+
+func _show_game_over() -> void:
+	tree.paused = true
+	%GameOver.visible = true
+#endregion
+
+
+func _switch_to_scene(scene_path: String) -> void:
+	tree.paused = false
+	tree.change_scene_to_file(scene_path)
+
+#region Ball
+func _check_collision(body: CollisionObject2D) -> void:
+	if not (body is Ground):
+		return
+	
+	a_hit_land.play()
+	_create_stuck_ball(ps_stuck_vb)
+		
+	_reduce_health(body.side)
+	SignalBus.landed.emit()
+
+func _create_stuck_ball(ps: PackedScene) -> void:
+	var pos = volleyball.global_position
+	var stuck_vb = ps.instantiate()
+	add_child(stuck_vb, true)
+
+	volleyball.queue_free()
+	volleyball = null
+	stuck_vb.global_position = pos	
+
 
 func _spawn_volleyball(ps: PackedScene) -> Volleyball:
 	var vb = ps.instantiate()
@@ -174,7 +212,9 @@ func _spawn_volleyball(ps: PackedScene) -> Volleyball:
 	vb.linear_velocity = Vector2.UP * 1000
 	
 	return vb
+#endregion
 
+#region Menu Actions
 func _on_try_again() -> void:
 	var t = %Fader.create_tween()
 	t.tween_property(%Fader, "color:a", 1, 2).set_ease(Tween.EASE_OUT)
@@ -185,6 +225,6 @@ func _on_try_again() -> void:
 
 	)
 
-
 func _on_return_to_menu() -> void:
 	get_tree().change_scene_to_file("res://Game/Menus/main_menu.tscn")
+#endregion
