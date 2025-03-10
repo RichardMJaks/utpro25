9855a0601543c92fbcb8b5a186221caef12f6370
RichardMJaks <richardmiikael.jaks@gmail.com>
2025-03-11 01:13:22 +0200

    Added dev shortcuts

---
+++ Game/Global/dev_shortcuts.gd
@@ -0,0 +1,8 @@
+extends Node
+
+signal global_lose()
+signal global_win()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("dev_menu"):
+		get_tree().change_scene_to_file("res://Game/Menus/main_menu.tscn")
