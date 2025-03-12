5a36f36915c26cac42c72f3baa205253c4ed38fb
Richard Miikael Jaks <richardmiikael.jaks@gmail.com>
2025-03-13 01:44:49 +0200

    Added win and lose dev shortcuts

--- Game/Global/dev_shortcuts.gd
+++ Game/Global/dev_shortcuts.gd
@@ -10,3 +10,9 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("dev_menu"):
 		get_tree().paused = false
 		get_tree().change_scene_to_file("res://Game/Menus/main_menu.tscn")
+	
+	if Input.is_action_just_pressed("dev_lose"):
+		global_lose.emit()
+	
+	if Input.is_action_just_pressed("dev_win"):
+		global_win.emit()
